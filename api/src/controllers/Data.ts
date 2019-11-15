951f76910dd5931245b9ec658d635dc3e1beafeb
Stanley Huang <huang.stanley050@gmail.com>
2019-11-15 19:55:58 +1100

    able to get result from unsplash api

--- api/src/controllers/Data.ts
+++ api/src/controllers/Data.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction, IRouter } from "express";
+import axios from "axios";
 import Photo from "../models/Photo";
 import { Controller } from "../App";
 import { Error } from "../App";
@@ -8,6 +9,7 @@ declare var process: {
     SPLASH_API_KEY: string;
   };
 };
+// https://api.unsplash.com/photos/random?&query=tokyo&count=2&client_id=b66dabcd8fc031f8cdbbdcd101b88f29effa08a4a232b034396faf2dd0fe24c2
 
 class DataController implements Controller {
   public path: string = "/data";
@@ -17,9 +19,26 @@ class DataController implements Controller {
   }
   initializeRoutes = () => {
     this.router.get(`${this.path}`, this.rootRoute);
+    this.router.get(`${this.path}/photos`, this.getPhotosRoute);
   };
   private rootRoute = (req: Request, res: Response) => {
     res.send("Data route");
+  };
+  private getPhotosRoute = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { keywords } = req.query;
+    const apiKey = process.env.SPLASH_API_KEY;
+    const queryString = `https://api.unsplash.com/photos/random?&count=4&query=${keywords}&client_id=${apiKey}`;
+    try {
+      let result = await axios.get(queryString);
+
+      res.json({ msg: "fetch success", result: result.data });
+    } catch (err) {
+      console.log(err);
+    }
   };
 }
 
