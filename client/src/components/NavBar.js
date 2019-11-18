98240b7106ba4833b28baad692a29afe9ffd598f
Stanley Huang <huang.stanley050@gmail.com>
2019-11-18 19:49:22 +1100

    links done in navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const NavBar = props => {
   return (
-    <div>
-      <h1>This is navbar</h1>
-    </div>
+    <Menu size="large" stackable>
+      <Menu.Item>
+        <img src="https://react.semantic-ui.com/logo.png" />
+      </Menu.Item>
+
+      <Menu.Item
+        name="login"
+        as={Link}
+        to="/login"
+        // active={activeItem === "features"}
+        // onClick={this.handleItemClick}
+      >
+        Login
+      </Menu.Item>
+
+      <Menu.Item
+        name="dashboard"
+        as={Link}
+        to="/dashboard"
+        // active={activeItem === "testimonials"}
+        // onClick={this.handleItemClick}
+      >
+        Dashboard
+      </Menu.Item>
+
+      <Menu.Item
+        name="photos"
+        as={Link}
+        to="/photos"
+        // active={activeItem === "sign-in"}
+        // onClick={this.handleItemClick}
+      >
+        Photos
+      </Menu.Item>
+    </Menu>
   );
 };
 
