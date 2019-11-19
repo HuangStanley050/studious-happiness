d48238d579c550c1613dd826c0ed94af02d1182c
Stanley Huang <huang.stanley050@gmail.com>
2019-11-19 21:30:16 +1100

    able to get the behavior of scrolling update, mimic with settimeout

--- client/src/components/InfinitePhotos.js
+++ client/src/components/InfinitePhotos.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Grid, Image } from "semantic-ui-react";
 import CardPhoto from "./CardPhoto";
 
 const InfinitePhotos = props => {
+  const [cards, setCards] = useState([
+    { name: "yo" },
+    { name: "tim" },
+    { name: "peter" }
+  ]);
+  const [length, setLengh] = useState(4);
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      setCards(cards.concat({ name: "hi" }));
+    }, 1500);
+  };
   return (
-    <Grid stackable container columns={2}>
-      <Grid.Column>
-        <CardPhoto />
-      </Grid.Column>
-      <Grid.Column>
-        <CardPhoto />
-      </Grid.Column>
-      <Grid.Column>
-        <CardPhoto />
-      </Grid.Column>
-      <Grid.Column>
-        <CardPhoto />
-      </Grid.Column>
-    </Grid>
+    <InfiniteScroll
+      dataLength={cards.length}
+      next={fetchMoreData}
+      hasMore
+      className="ui stackable two column grid"
+      loader={<h4>Loading...</h4>}
+    >
+      {cards.map((card, index) => {
+        return (
+          <Grid.Column>
+            <CardPhoto key={card.name} />;
+          </Grid.Column>
+        );
+      })}
+    </InfiniteScroll>
   );
 };
 
 export default InfinitePhotos;
+
+// <Grid.Column>
+//   <CardPhoto />
+// </Grid.Column>
+// <Grid.Column>
+//   <CardPhoto />
+// </Grid.Column>
+// <Grid.Column>
+//   <CardPhoto />
+// </Grid.Column>
+// <Grid.Column>
+//   <CardPhoto />
+// </Grid.Column>
